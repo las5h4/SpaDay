77c6d4bbdaedb51da079f8a6757563c56c975a76
las5h4 <loganasmalley@gmail.com>
2020-11-17 14:46:50 -0600

    Added ViewModel with validation attributes

---
+++ SpaDay/ViewModels/AddUserViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace SpaDay.ViewModels
+{
+    public class AddUserViewModel
+    {
+        [Required(ErrorMessage = "Username is required!")]
+        [StringLength(15, MinimumLength = 5, ErrorMessage = "Username must be between 5 and 15 characters!")]
+        public string Username { get; set; }
+
+        [EmailAddress(ErrorMessage = "Not a valid email address. If you do not wish to submit an email address, leave this field blank.")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Password is required!")]
+        [StringLength(20, MinimumLength = 6, ErrorMessage = "Password must be bettween 6 and 20 characters!")]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "Please verify your password!")]
+        public string VerifyPassword { get; set; }
+    }
+}
